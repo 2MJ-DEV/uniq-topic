aa41790665c0dbfa0ae2ca93d5b531612105ba26
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-03-20 12:37:08 +0100

    Création de la page login et ajout du formulaire de connexion

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+
+  
+  build: {
+    rollupOptions: {
+        input: {
+            main: resolve(__dirname, '/index.html'),
+            login: resolve(__dirname, 'src/pages/login.html'),
+            register: resolve(__dirname, 'src/pages/register.html'),
+            dashboard: resolve(__dirname, 'src/pages/dashboard.html'),
+            submitSubject: resolve(__dirname, 'src/pages/submitSubject.html'),
+        },
+    },
+},
+
 })
